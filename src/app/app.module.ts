ecb69a9136d9f38fbe826c672119db85768f1f45
matszc <matiz305@hotmail.com>
2019-07-26 23:44:23 +0200

    create sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateLobbyComponent } from './components/create-lobby/create-lobby.component';
+import { DecodeComponent } from './components/decode/decode.component';
+import { HomeComponent } from './components/home/home.component';
+import { JoinLobbyComponent } from './components/join-lobby/join-lobby.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateLobbyComponent,
+    DecodeComponent,
+    HomeComponent,
+    JoinLobbyComponent,
+    NavMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'decode', component: DecodeComponent },
+      { path: 'join', component: JoinLobbyComponent },
+      { path: 'create', component: CreateLobbyComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config.ts
@@ -0,0 +1,3 @@
+export const GLOBAL = {
+  URL: 'https://localhost:44381/api'
+};
